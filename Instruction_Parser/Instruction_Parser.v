efcc9996893f868dbb459762b17b191ef6826c37
Jonah Spear <Jonahspear512@gmail.com>
2017-11-08 18:35:44 -0500

    Add Instruction Parser Shell

---
+++ Instruction_Parser/Instruction_Parser.v
@@ -0,0 +1,11 @@
+// Instruction Parser parses the relevant data and control signals from the instruction.
+
+module Instruction_Parser
+(
+    input PC,
+    // Instruction values
+    output[5:0] Rs, Rd, Rt,
+    output[15:0] imm,
+    output[25:0] addr,
+    output ALUCtrl, MemToReg, MemWr, ALUSrc, PCSel, RegDst, RegWr, AddSel
+)
