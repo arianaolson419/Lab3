0e9e79f80c52a6fcc8b61c58ce8ed3266676cc30
arianaolson419 <arianaolson419@gmail.com>
2017-11-07 22:39:16 -0500

    Add inputs and outputs to module definition

--- core.v
+++ core.v
@@ -3,7 +3,14 @@ The section of the CPU containing the register file, data memory, and ALU.
 */
 
 module core (
-
+	input clk,
+	input [31:0] rd,
+	input [31:0] rt,
+	input [31:0] rs,
+	input [15:0] immediate,
+	input [31:0] added_PC,
+	output [31:0] Da,
+	output is_zero
 );
 
 // code here
