80343e8726f0c67a253738ac8459a50fb1f3e36d
arianaolson419 <arianaolson419@gmail.com>
2017-11-08 19:29:14 -0500

    Add new modules

--- core.t.v
+++ core.t.v
@@ -9,13 +9,19 @@ module core_TEST();
 	wire is_zero;
 
 	reg clk;
-	reg [31:0] rd;
-	reg [31:0] rt;
-	reg [31:0] rs;
+	reg [5:0] rd;
+	reg [5:0] rt;
+	reg [5:0] rs;
 	reg [15:0] immediate;
-	reg [31:0] added_PC;
+	reg [31:0] new_PC;
+	reg [0:1] regdst;
+	reg [2:0] ALUcntrl;
+	reg AlUsrc;
+	reg MemWr;
+	reg RegWr;
+	reg [1:0] MemtoReg;
 
-	core dut (clk, rd, rt, rs, immediate, added_PC, Da, is_zero);
+	core dut (clk, rd, rt, rs, immediate, new_PC, regdst, ALUcntrl, AlUsrc, MemWr, RegWr, MemtoReg, Da, is_zero);
 
 	initial begin
 		// Test code here.
