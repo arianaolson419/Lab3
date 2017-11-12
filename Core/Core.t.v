8be98892718a75f20a0c990dc1d87d1e15f64698
arianaolson419 <arianaolson419@gmail.com>
2017-11-11 19:45:38 -0500

    Add remaining tests

--- Core/Core.t.v
+++ Core/Core.t.v
@@ -43,94 +43,242 @@ module Core_TEST();
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd2; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd3; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd4; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd5; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd6; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd7; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd8; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd9; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd10; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd11; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd12; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd13; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd14; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd15; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd16; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd17; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd18; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd19; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd20; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd21; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd22; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd23; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd24; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd25; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd26; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd27; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd28; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd29; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd30; Rs = 5'b00000; imm = 16'b0;
 		#6000
+
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
+
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
 		Rd =5'bx; Rt = 5'd31; Rs = 5'b00000; imm = 16'b0;
 		#6000
 
+		if (dut.databout !== 32'b0) begin
+			$display("Test case 1 failed: R[rt] != R[rs] + 32'b0 at time %t", $time);
+		end
 
 		// Test Case 2: Load Word
 		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 2'b01;
@@ -150,15 +298,132 @@ module Core_TEST();
 			$display("Test case 3 failed: M[R[rs] + signextendimm] != R[rt] at time %t", $time);
 		end
 
-		// Test Case 4: Jump
-		// Test Case 5: Jump Register
-		// Test Case 6: Jump and Link
-		// Test Case 7: Branch if Not Equal
-		// Test Case 8: XOR Immediate
-		// Test Case 9: Add
-		// Test Case 10: Subtract
-		// Test Case 11: Set Less Than
-		#10000
+		// Test Case 4: Jump and Link
+		RegDst = 2'b10; AlUSrc = 1'bx; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'bx; MemToReg = 2'b10;
+		Rd = 5'bx; Rs = 5'd31; Rt = 5'bx; imm = 16'bx; addedPC = 5'd25;
+		#1000
+
+		if (Da !== addedPC) begin
+			$display("Test case 4 failed: PC value not saved to correct register at time %t", $time);
+		end
+
+		#1000
+
+		// Test Case 5: Branch if Not Equal
+		// Load Immediate into registers 16 and 17 so that they are not equal.
+		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
+		Rd =5'bx; Rt = 5'd16; Rs = 5'b00000; imm = 16'd16;	// R[16] = 0 + 16 = 16
+		#6000
+		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
+		Rd =5'bx; Rt = 5'd17; Rs = 5'b00000; imm = 16'd17;	// R[17] = 0 + 17 = 17
+		#6000
+
+		RegDst = 2'bx; AlUSrc = 1'b0; RegWr = 1'b0; MemWr = 1'b0; ALUCntrl = 3'b001; MemToReg = 2'bx;
+		Rd = 5'dx; Rs = 5'd16; Rt = 5'd17; imm = 16'bx;
+		#6000
+
+		if (dut.regfile.register16out === dut.regfile.register17out) begin
+			$display("Error: R[rs] should not equal R[rt]");
+		end
+
+		if (isZero === 1'b1) begin
+			$display("Test case 5 failed: R[Rs] == R[Rt] indicated, but these values are not equal at time %t", $time);
+		end
+
+		// Load Immediate into registers 16 and 17 so that they are equal.
+		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
+		Rd =5'bx; Rt = 5'd16; Rs = 5'b00000; imm = 16'd15;	// R[16] = 0 + 15 = 15
+		#6000
+		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
+		Rd =5'bx; Rt = 5'd17; Rs = 5'b00000; imm = 16'd15;	// R[17] = 0 + 15 = 15
+		#6000
+
+		RegDst = 2'bx; AlUSrc = 1'b0; RegWr = 1'b0; MemWr = 1'b0; ALUCntrl = 3'b001; MemToReg = 2'bx;
+		Rd = 5'dx; Rs = 5'd16; Rt = 5'd17; imm = 16'bx;
+		#6000
+
+		if (dut.regfile.register16out !== dut.regfile.register17out) begin
+			$display("Error: R[rs] should equal R[rt]");
+		end
+
+		if (isZero === 1'b0) begin
+			$display("Test case 5 failed: R[Rs] != R[Rt] indicated, but these values are equal at time %t", $time);
+		end
+
+		// Test Case 6: XOR Immediate
+		// Load Immediate into register 17
+		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
+		Rd =5'bx; Rt = 5'd17; Rs = 5'b00000; imm = 16'h00005555;
+		#6000
+
+		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b010; MemToReg = 2'b0;
+		Rd = 5'dx; Rs = 5'd17; Rt = 5'd16; imm = 16'h00005555;
+		#6000
+
+		if (dut.databout != 32'b0) begin
+			$display("Test case 6 failed: R[Rs] == signextendimm and R[Rs] XOR signextendimm != 32'b0 at time %t", $time);
+		end
+
+		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b010; MemToReg = 2'b0;
+		Rd = 5'dx; Rs = 5'd17; Rt = 5'd16; imm = 16'h0000aaaa;
+		#6000
+
+		if (dut.databout != 32'h0000003f) begin
+			$display("Test case 6 failed: R[Rs] != signextendimm and R[Rs] XOR signextendimm != 32'h0000ffff at time %t", $time);
+		end
+
+		// Test Case 7: Add
+		// Load Immediate into register 20
+		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
+		Rd =5'bx; Rt = 5'd20; Rs = 5'b00000; imm = 16'd25;
+		#6000
+
+		// Load Immediate into register 3
+		RegDst = 2'b01; AlUSrc = 1'b1; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 1'b0;
+		Rd =5'bx; Rt = 5'd3; Rs = 5'b00000; imm = 16'd19;
+		#6000
+
+		RegDst = 2'b0; AlUSrc = 1'b0; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b000; MemToReg = 2'b0;
+		Rd = 5'd5; Rs = 5'd20; Rt = 5'd3; imm = 16'bx;
+		#6000
+
+		if (dut.regfile.register5out !== 32'd44) begin
+			$display("Test case 7 failed: R[Rd] != R[Rs] + R[Rt] at time %t", $time);
+		end
+
+		// Test Case 8: Subtract
+		RegDst = 2'b0; AlUSrc = 1'b0; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b001; MemToReg = 2'b0;
+		Rd = 5'd5; Rs = 5'd20; Rt = 5'd3; imm = 16'bx;
+		#6000
+
+		if (dut.regfile.register5out !== 32'd6) begin
+			$display("Test case 8 failed: R[Rd] != R[Rs] - R[Rt] at time %t", $time);
+		end
+
+		// Test Case 9: Set Less Than
+		RegDst = 2'b0; AlUSrc = 1'b0; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b011; MemToReg = 2'b0;
+		Rd = 5'd5; Rs = 5'd20; Rt = 5'd3; imm = 16'bx;
+		#6000
+
+		if (dut.regfile.register5out !== 32'b0) begin
+			$display("Test case 9 failed: R[Rd] is not zero, but R[Rs] !< R[Rt] at time %t", $time);
+		end
+
+		RegDst = 2'b0; AlUSrc = 1'b0; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b011; MemToReg = 2'b0;
+		Rd = 5'd5; Rs = 5'd3; Rt = 5'd20; imm = 16'bx;
+		#6000
+
+		if (dut.regfile.register5out !== 32'b1) begin
+			$display("Test case 9 failed: R[Rd] is not one, but R[Rs] < R[Rt] at time %t", $time);
+		end
+
+		RegDst = 2'b0; AlUSrc = 1'b0; RegWr = 1'b1; MemWr = 1'b0; ALUCntrl = 3'b011; MemToReg = 2'b0;
+		Rd = 5'd5; Rs = 5'd20; Rt = 5'd20; imm = 16'bx;
+		#6000
+
+		if (dut.regfile.register5out !== 32'b0) begin
+			$display("Test case 9 failed: R[Rd] is not zero, but R[Rs] == R[Rt] at time %t", $time);
+		end
 
 		$finish;
 	end
