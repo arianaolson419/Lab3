2460baba0adc463de039617423648836401cd3b6
Jonah Spear <Jonahspear512@gmail.com>
2017-11-14 12:16:12 -0500

    Run tests without ./run_program
    test now uses writememh to write directly to mem.dat instead of having to use
    a wrapper function to write into it. The helper function has been deleted because
    it is no longer necessary.

--- CPU.t.v
+++ CPU.t.v
@@ -5,6 +5,7 @@
 module testCPU ();
 
 	reg CLK;
+	reg[31:0] mem [16:0];
 
 	CPU dut (.CLK(CLK));
 
@@ -13,6 +14,10 @@ module testCPU ();
 	initial begin
 		$dumpfile("CPU.vcd");
 		$dumpvars;
+
+		mem[0] = 32'h2008_0001;
+		mem[1] = 32'h2008_0002;
+		$writememh("mem.dat", mem);
 		#6000;
 		$display("Program counter: %h", dut.PC);
 		CLK = 1; #6000
