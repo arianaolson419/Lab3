f4d10b945a693f9fcede2cb0a48e28cbbbce8826
arianaolson419 <arianaolson419@gmail.com>
2017-11-14 19:46:51 -0500

    Change test instructions for diagnostics

--- AssemblyPrograms/testCPU.asm
+++ AssemblyPrograms/testCPU.asm
@@ -1,7 +1,14 @@
 # Test commands for the CPU
-main:
+testaddi:
 addi $t0, $zero, 1
 addi $t1, $zero, 10
+j testslt
+
+addi $t1, $zero, 4
+
+testslt:
+#sw $t0, 0($sp)
+addi $t1, $zero, 4
 j end
 
 end:
