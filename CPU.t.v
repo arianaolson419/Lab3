9f882b657afdf7b89858f67c4da44e243654d999
arianaolson419 <arianaolson419@gmail.com>
2017-11-14 19:50:26 -0500

    Diasble memory overwrite and add clock cycles

--- CPU.t.v
+++ CPU.t.v
@@ -11,30 +11,33 @@ module testCPU ();
 
 	initial begin
 		$dumpfile("CPU.vcd");
-		$dumpvars;
+		$dumpvars(0, testCPU, dut.c.datamemory.memory[0]);
 
 		//add $t1, $zero, $zero
-		mem[0] = 32'h00004820;
-		$writememh("mem.dat", mem);
+		// mem[0] = 32'h00004820;
+		// $writememh("mem.dat", mem);
 		CLK = 1; #12000; CLK = 0; #12000;
 		$display("$t1: %h", dut.c.regfile.register9out);
 		$display("Program counter: %h", dut.PC);
 
 		//addi $t1, $zero, 2
-		mem[1] = 32'h20090002;
-		$writememh("mem.dat", mem);
+		// mem[1] = 32'h20090002;
+		// $writememh("mem.dat", mem);
 		CLK = 1; #12000; CLK = 0; #12000;
 		$display("$t1: %h", dut.c.regfile.register9out);
 		$display("Program counter: %h", dut.PC);
 
 		//sw $t1, 0
-		mem[2] = 32'hac090000;
-		$writememh("mem.dat", mem);
+		// mem[2] = 32'hac090000;
+		// $writememh("mem.dat", mem);
 		CLK = 1; #12000; CLK = 0; #12000;
 		$display("M[0]: %h", dut.c.datamemory.memory[0]);
 		$display("Program counter: %h", dut.PC);
 
 
+		CLK = 1; #12000; CLK = 0; #12000;
+		CLK = 1; #12000; CLK = 0; #12000;
+		CLK = 1; #12000; CLK = 0; #12000;
 
 	end
 
