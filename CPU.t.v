eec285fd9ea9d3ccab704d67d1c92a6eebf71cfe
Jonah Spear <Jonahspear512@gmail.com>
2017-11-14 19:14:55 -0500

    Make tests work

--- CPU.t.v
+++ CPU.t.v
@@ -16,15 +16,22 @@ module testCPU ();
 		//add $t1, $zero, $zero
 		mem[0] = 32'h00004820;
 		$writememh("mem.dat", mem);
-		CLK = 1; #6000; CLK = 0; #6000
-		$display("$t0: %h", dut.c.regfile.register9out);
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("$t1: %h", dut.c.regfile.register9out);
 		$display("Program counter: %h", dut.PC);
 
 		//addi $t1, $zero, 2
-		mem[1] = 32'h02009002;
+		mem[1] = 32'h20090002;
 		$writememh("mem.dat", mem);
-		CLK = 1; #6000; CLK = 0; #6000
-		$display("$t0: %h", dut.c.regfile.register9out);
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("$t1: %h", dut.c.regfile.register9out);
+		$display("Program counter: %h", dut.PC);
+
+		//sw $t1, 0
+		mem[2] = 32'hac090000;
+		$writememh("mem.dat", mem);
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("M[0]: %h", dut.c.datamemory.memory[0]);
 		$display("Program counter: %h", dut.PC);
 
 
