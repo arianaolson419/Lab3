24a95258d913c04e521fd2d2654ca8ae72591564
apan64 <andrew.pan@students.olin.edu>
2017-11-16 18:28:26 -0500

    set CPU.t.v back to unit tests

--- CPU.t.v
+++ CPU.t.v
@@ -59,308 +59,171 @@ module testCPU ();
 		// PC = 0
 		CLK = 1; #12000; CLK = 0; #12000;
 		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
+		if (dut.c.regfile.register8out != 32'd5) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd5, dut.c.regfile.register8out);
+		end
+		
+		// PC = 4
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register9out != 32'd130) begin
+			$display("Error at register $t1. Expected value: %h, actual value: %h", 32'd130, dut.c.regfile.register9out);
+		end
+
+		// PC = 8
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register8out != 32'd9) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd9, dut.c.regfile.register8out);
+		end
+
+		// xori test cases
+		CLK = 1; #12000; CLK = 0; #12000;
+		CLK = 1; #12000; CLK = 0; #12000;
+		CLK = 1; #12000; CLK = 0; #12000;
+
+		// PC = 24
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register8out != 32'd5) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd5, dut.c.regfile.register8out);
+		end
+
+		// PC = 28
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register8out != 32'd0) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd0, dut.c.regfile.register8out);
+		end
 
 		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
+		CLK = 1; #12000; CLK = 0; #12000;
+		CLK = 1; #12000; CLK = 0; #12000;
+		CLK = 1; #12000; CLK = 0; #12000;
+		CLK = 1; #12000; CLK = 0; #12000;
+		CLK = 1; #12000; CLK = 0; #12000;
+		CLK = 1; #12000; CLK = 0; #12000;
+
+		// PC = 60
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register8out != 32'd63) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd63, dut.c.regfile.register8out);
+		end
+
+		// PC = 64
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register8out != 32'd11) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd11, dut.c.regfile.register8out);
+		end
+
+		// PC = 68
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register8out != -32'd16) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", -32'd16, dut.c.regfile.register8out);
+		end
 
 		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
+
+		// PC = 76
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register8out != -32'd51) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", -32'd51, dut.c.regfile.register8out);
+		end
+
+		// PC = 80
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register8out != -32'd29) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", -32'd29, dut.c.regfile.register8out);
+		end
+
+		// PC = 84
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register8out != 32'd6) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd6, dut.c.regfile.register8out);
+		end
 
 		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
+
+		// PC = 92
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register8out != 32'd1) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd1, dut.c.regfile.register8out);
+		end
+
+		// PC = 96
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register8out != 32'd1) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd1, dut.c.regfile.register8out);
+		end
+
+		// PC = 100
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register8out != 32'd0) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd0, dut.c.regfile.register8out);
+		end
 
 		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
+		CLK = 1; #12000; CLK = 0; #12000;
+		CLK = 1; #12000; CLK = 0; #12000;
+
+		// PC = 124
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register11out != 32'd1) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd1, dut.c.regfile.register11out);
+		end
 
 		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
+		CLK = 1; #12000; CLK = 0; #12000;
+
+		// PC = 124
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register11out != 32'd2) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd2, dut.c.regfile.register11out);
+		end
 
 		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
+		CLK = 1; #12000; CLK = 0; #12000;
+
+		// PC = 124
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register11out != 32'd3) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd3, dut.c.regfile.register11out);
+		end
 
 		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
+		CLK = 1; #12000; CLK = 0; #12000;
+
+		// PC = 124
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register11out != 32'd4) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd4, dut.c.regfile.register11out);
+		end
 
 		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
+		CLK = 1; #12000; CLK = 0; #12000;
+
+		// PC = 116
+		CLK = 1; #12000; CLK = 0; #12000;
+		$display("Program counter: %h", dut.PC);	
+		if (dut.c.regfile.register13out != 32'd4) begin
+			$display("Error at register $t0. Expected value: %h, actual value: %h", 32'd4, dut.c.regfile.register8out);
+		end
 
 		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
 		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
-
-		CLK = 1; #12000; CLK = 0; #12000;
-		$display("Program counter: %h", dut.PC);
-		$display("$ra: %h", dut.c.regfile.register31out);
-		$display("Memtoreg mux output: %h", dut.c.regdatamux.out);
 
 	end
 
