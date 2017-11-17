1ee2cac8656add43a7e58cd907d92bb2ae063dd1
apan64 <andrew.pan@students.olin.edu>
2017-11-16 22:32:05 -0500

    test case only displays on fail

--- PC_Calc/PC_Calc.t.v
+++ PC_Calc/PC_Calc.t.v
@@ -17,7 +17,6 @@ module testPCCalc();
 	
 	PC_Calc pccalc(old_PC[31:0], isZero, PCSel[1:0], AddSel, Da[31:0], addr[25:0], imm[15:0], added_PC[31:0], new_PC[31:0]);
 	initial begin
-		$display("added_PC                         | new_PC");
 		old_PC = 32'd4;
 		isZero = 0;
 		PCSel = 0;
@@ -25,8 +24,10 @@ module testPCCalc();
 		Da = 32'd16;
 		addr = 26'd12;
 		imm = 16'd10; #1000
-		$display("expected 8 and 48");
-		$display("%b | %b", added_PC, new_PC);
+		if (added_PC != 32'd8 || new_PC != 32'd48) begin
+			$display("test case failed, expected 8 and 48");
+			$display("added_PC: %b | new_PC: %b", added_PC, new_PC);
+		end		
 
 		old_PC = 32'b01010000000000000000000000000000;
 		isZero = 0;
@@ -35,7 +36,9 @@ module testPCCalc();
 		Da = 32'd16;
 		addr = 26'd12;
 		imm = 16'd10; #1000
-		$display("expected 01010000000000000000000000000100 and 01010000000000000000000000110000");
-		$display("%b | %b", added_PC, new_PC);
+		if (added_PC != 32'd8 || new_PC != 32'd48) begin
+			$display("test case failed, expected 01010000000000000000000000000100 and 01010000000000000000000000110000");
+			$display("added_PC: %b | new_PC: %b", added_PC, new_PC);
+		end		
 	end
 endmodule
