101fddee855b3ec60bec55bc765c94a433f5d2e1
arianaolson419 <arianaolson419@gmail.com>
2017-11-16 22:09:12 -0500

    Create bash script to run tests

---
+++ run_tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+make
+
+java -jar ../Mars4_5.jar a dump .text HexText fib.text mc CompactTextAtZero AssemblyPrograms/fibonacci.asm
+
+cat fib.text > mem.dat
+
+./CPU_fib
+
+java -jar ../Mars4_5.jar a dump .text HexText while_loop.text mc CompactTextAtZero AssemblyPrograms/testCPU.asm
+
+cat while_loop.text > mem.dat
+
+./CPU_while
+
+java -jar ../Mars4_5.jar a dump .text HexText combined_unit_tests.text mc CompactTextAtZero AssemblyPrograms/combined_unit_tests.asm
+
+cat combined_unit_tests.text > mem.dat
+
+./CPU_unit
+
+./Core/Core
+./Core/ALU/alu
+./Core/regfile
+./PC_Calc/PC_Calc
+./Instruction_Parser/Instruction_Parser
